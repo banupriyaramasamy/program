5ae2c7af75a13800bb85014b95fbef5d5b2fb797
banupriyaramasamy <46838674+banupriyaramasamy@users.noreply.github.com>
2019-02-08 01:04:37 +0530

    set5 4.c

---
+++ set5 4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int a,i,c=0;
+ scanf("%d",&a);
+     if(a>=1 && a<=10)
+     {
+
+     printf("Yes");
+     }
+     else
+     {
+         printf("No");
+     }
+    return 0;
+}
