8dc41d7d6f6c7dbb8a4c258d9c75ff9884102b22
banupriyaramasamy <46838674+banupriyaramasamy@users.noreply.github.com>
2019-02-08 21:52:09 +0530

    set5 7.c

---
+++ set5 7.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+{
+int n,a[10],i,max=0,min=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+        if(a[i]<min)
+        {
+            min=a[i];
+        }
+    }
+    printf("%d %d",min ,max);
+    return 0;
+    }
