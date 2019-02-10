0ad6b5458e457ae2a787dbfd4a8b2ffc424181db
banupriyaramasamy <46838674+banupriyaramasamy@users.noreply.github.com>
2019-02-10 23:08:21 +0530

    set9 7.c

---
+++ set9 7.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,g=0;
+
+scanf("%d %d",&a,&b);
+g=(a<b)?a:b;
+while(1)
+{
+    if(a%g==0 && b%g==0)
+    {
+        printf("%d",g);
+        break;
+    }
+    g--;
+}
+return 0;
+}
