637e2492655b4a1e8130d98d7b12bbb8d1fa3129
banupriyaramasamy <46838674+banupriyaramasamy@users.noreply.github.com>
2019-02-10 22:08:51 +0530

    set8 8.c

---
+++ set8 8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    
+    int n,i;
+    scanf("%d",&n);
+    if(n%13==0)
+    {
+    printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+
+    return 0;
+}
