72cec6983fe63b3c692c7da931ca0f08c357cb0b
banupriyaramasamy <46838674+banupriyaramasamy@users.noreply.github.com>
2019-02-11 00:06:49 +0530

    set10 8.c

---
+++ set10 8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n,a[10],i;
+    scanf("%d ",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i !=a[i])
+        {
+            printf("%d",i);
+        }
+    }
+    
+    
+return 0;
+}
