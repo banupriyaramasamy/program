10a9f5cad8a1fff2c4767a4817f85b91ccc8984f
banupriyaramasamy <46838674+banupriyaramasamy@users.noreply.github.com>
2019-02-10 20:48:50 +0530

    set7 9.c

---
+++ set7 9.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,m,a;
+    scanf("%d %d",&n,&m);
+    a=n-m;
+    if(a%2==0)
+    {
+        printf("Even");
+    }
+    else
+    {
+printf("Odd");
+}
+    return 0;
+}
