6c1b10fc0444d70fa6586861f164d3cdb4150fb0
banupriyaramasamy <46838674+banupriyaramasamy@users.noreply.github.com>
2019-02-10 21:25:19 +0530

    set8 2.c

---
+++ set8 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char n[20];
+    int i,m=0,c=0;
+    scanf("%s",n);
+    if(n[i]=='a' || n[i]=='e' || n[i]=='i' || n[i]=='o' || n[i]=='u' )
+    {
+    printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    return 0;
+}
