5e53307b1779deceb75d2813d7d4f4dd6a9b101d
banupriyaramasamy <46838674+banupriyaramasamy@users.noreply.github.com>
2019-02-10 21:38:03 +0530

    set8  3.c

---
+++ set8 3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+
+    int m,c,n;
+    scanf("%d %d %d",&c,&n,&m);
+    if(c>n && c<m )
+    {
+    printf("Yes");
+    }
+    else
+    {
+        printf("No");
+    }
+    return 0;
+}
