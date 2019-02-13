44e8e49255c080d0d95702470ddb9a5d01fff83b
banupriyaramasamy <46838674+banupriyaramasamy@users.noreply.github.com>
2019-02-13 21:47:56 +0530

    set11 2.c

---
+++ set11 2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  int a,b;
+    scanf("%d",&a);
+        if(a%2==0)
+        {
+            b=a/2;
+            printf("%d",b);
+        }
+        else
+        {
+    printf("%d",a);
+        }
+
+    return 0;
+}
