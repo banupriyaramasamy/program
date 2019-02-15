fd1770f366f1a00f09ae8bdbf64e54386cdc2e4e
banupriyaramasamy <46838674+banupriyaramasamy@users.noreply.github.com>
2019-02-15 22:06:30 +0530

    set11 4.c

---
+++ set11 4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    int c=0;
+    scanf("%d %d",&a,&b);
+    c=b;
+    while(b!=0)
+    {
+       a*=10;
+        b /=10;
+    }
+           
+        printf("%d",a+c);
+    
+
+    return 0;
+}
