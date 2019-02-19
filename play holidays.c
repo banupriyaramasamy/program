c77dfa989d562efd0d4016daa5c6b2cee5f3d6fa
banupriyaramasamy <46838674+banupriyaramasamy@users.noreply.github.com>
2019-02-19 15:42:28 +0530

    play holidays.c

---
+++ play holidays.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+ char a[10];
+    scanf("%s",a);
+    if(a[0]=='s' || a[0]=='S')
+    {
+        printf("yes");
+    }
+    else
+    {
+    printf("no");
+}
+    return 0;
+}
