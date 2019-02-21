e3dcb52d67752c8f19652d3f8f815ddf14295ef6
banupriyaramasamy <46838674+banupriyaramasamy@users.noreply.github.com>
2019-02-21 14:59:17 +0500

    playset2right.c

---
+++ playset2right.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,k,a[10],i,r;
+    scanf("%d %d",&n,&k);
+    for(i=0;i<n;i++)
+{
+    scanf("%d",&a[i]);
+}
+while(k)
+{
+    r=a[n-1];
+    for(i=n-1;i>=0;i--)
+    {
+        a[i]=a[i-1];
+    }
+    a[0]=r;
+    k--;
+}
+for(i=0;i<n;i++)
+{
+ printf("%d",a[i]);
+}
+    return 0;
+}
