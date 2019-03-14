3ca03e21abe864d1242593cfd03707b7ad86c940
banupriyaramasamy <46838674+banupriyaramasamy@users.noreply.github.com>
2019-03-14 19:59:40 +0530

    arrayn.c

---
+++ arrayn.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+     int n,a[10],k,i,j,t;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i;j<n;j++)
+        {
+            if(a[i]>a[j])
+            {
+                t=a[i];
+                a[i]=a[j];
+                a[j]=t;
+            }
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<n)
+        {
+           printf("%d ",a[i]);
+        }
+    }
+
+
+    return 0;
+}
